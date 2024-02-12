76280cccce2f0e1c1efd566a8f231dd35e99e86d
IldarAkhmet <145066478+IldarAkhmet@users.noreply.github.com>
2024-02-12 15:01:15 +0300

    hide database password

--- API/database.py
+++ API/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQL_DATABASE_URL = r"postgresql://postgres:19281928@localhost/Houses"
+SQL_DATABASE_URL = r"postgresql://postgres:password@localhost/Houses"
 
 engine = create_engine(SQL_DATABASE_URL) # движок базы данных алхимии
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # фабрика сессий для взаимодействия с базой данных
